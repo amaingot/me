bfb0355bea0b9760ff685e177bc61e0462df5a4f
Alex Maingot <alex.maingot@gmail.com>
2021-01-04 09:36:01 +0000

    Fix deploy

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,6 +1,6 @@
 import expressWinston from "express-winston";
 import winston from "winston";
-import config from "./config";
+// import config from "./config";
 
 const transports: any[] = [];
 
@@ -9,10 +9,10 @@ transports.push(new winston.transports.Console());
 export const logger = winston.createLogger({
   level: "info",
   transports,
-  format:
-    config.get("NODE_ENV") !== "production"
-      ? winston.format.simple()
-      : undefined,
+  // format:
+  //   config.get("NODE_ENV") !== "production"
+  //     ? winston.format.simple()
+  //     : undefined,
 });
 
 export const appLogger = () =>
@@ -20,7 +20,7 @@ export const appLogger = () =>
     winstonInstance: logger,
     level: "info",
     meta: true,
-    colorize: config.get("NODE_ENV") !== "production",
+    // colorize: config.get("NODE_ENV") !== "production",
   });
 
 export const appErrorLogger = () =>
